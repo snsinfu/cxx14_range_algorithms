e4a885612ead9e16d02d872ea9abafcc4cb17fc9
snsinfu <snsinfu@gmail.com>
2018-03-23 17:46:28 +0900

    Update copyright

--- range.hpp
+++ range.hpp
@@ -1,7 +1,7 @@
 /*
  * Range-adapted standard algorithms for C++14
  *
- * Copyright frickiericker 2016.
+ * Copyright snsinfu 2016-2018.
  * Distributed under the Boost Software License, Version 1.0.
  *
  * Permission is hereby granted, free of charge, to any person or organization
